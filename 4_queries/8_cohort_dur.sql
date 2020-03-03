a875f23d5ed24f7dfcc6ef0e9c2cd377a0ca5389
Artur Iatsko <arturyatsko@gmail.com>
2020-03-03 22:47:24 +0000

    Total cohort duration added

--- 4_queries/8_cohort_dur.sql
+++ 4_queries/8_cohort_dur.sql
@@ -1,7 +1,6 @@
-SELECT cohorts.name AS name, avg(completed_at - started_at) AS average_assistance_time
+SELECT cohorts.name AS name, sum(completed_at - started_at) AS total_duration
 FROM assistance_requests
   JOIN students ON students.id = assistance_requests.student_id
   JOIN cohorts ON cohorts.id = students.cohort_id
 GROUP BY cohorts.name
-ORDER BY average_assistance_time DESC
-LIMIT 1;
+ORDER BY total_duration;
