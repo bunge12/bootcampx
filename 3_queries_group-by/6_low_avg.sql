2707a1be3ae9d4530c8420b02d3c1236e77114c8
Artur Iatsko <arturyatsko@gmail.com>
2020-03-03 01:08:07 +0000

    Low Avg Completion added

---
+++ 3_queries_group-by/6_low_avg.sql
@@ -0,0 +1,8 @@
+SELECT students.name AS student, avg(assignment_submissions.duration) AS average_assignment_duration, avg(assignments.duration) AS average_estimated_duration
+FROM assignment_submissions
+  JOIN students ON assignment_submissions.student_id = students.id
+  JOIN assignments ON assignment_submissions.assignment_id = assignments.id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration
